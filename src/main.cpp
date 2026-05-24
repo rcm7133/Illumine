a3fd74f41aac287c3c4203b7ee41a86b4de1edb8
rcm7133 <moorma14@msu.edu>
2026-05-24 14:17:09 -0400

    Added Vulkan support

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
+#include "pch.h"
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    uint32_t version;
+    vkEnumerateInstanceVersion(&version);
+    std::cout << "Vulkan version: "
+              << VK_VERSION_MAJOR(version) << "."
+              << VK_VERSION_MINOR(version) << "."
+              << VK_VERSION_PATCH(version) << "\n";
     return 0;
 }
